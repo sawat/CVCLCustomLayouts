9bd35233c27da5a84412f687a4198aef20ed6c58
sawat <sawat1203@gmail.com>
2012-11-11 03:47:55 +0900

    Delete Button View

--- Examples/CVCLEamaples/MyCollectionViewCell.m
+++ Examples/CVCLEamaples/MyCollectionViewCell.m
@@ -10,6 +10,14 @@
 
 #import <QuartzCore/QuartzCore.h>
 
+@interface DeleteButtonView : UIView
+@end
+
+@interface MyCollectionViewCell ()
+@property (nonatomic, weak) DeleteButtonView * deleteButtonView;
+@end
+
+#pragma mark -
 
 @implementation MyCollectionViewCell
 
@@ -42,21 +50,72 @@
     
     if (editing) {
 
-        CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform.rotation.z"];
-        
-        animation.duration = 0.5f;
-        animation.repeatCount = HUGE_VAL;
-        
-        CGFloat digree = M_PI / 180 * 3;
-        
-        animation.values = @[@0, @(digree), @0, @(-digree), @(0)];
-        
-        [self.layer addAnimation:animation forKey:animationKey];
+        if ([self.layer animationForKey:animationKey] == nil) {
+            CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform.rotation.z"];
+            
+            animation.duration = 0.5f;
+            animation.repeatCount = HUGE_VAL;
+            
+            CGFloat digree = M_PI / 180 * 3;
+            
+            animation.values = @[@0, @(digree), @0, @(-digree), @(0)];
+            
+            [self.layer addAnimation:animation forKey:animationKey];
+        }
+
+        if (self.deleteButtonView == nil) {
+            DeleteButtonView *view = [[DeleteButtonView alloc] initWithFrame:self.bounds];
+            [self addSubview:view];
+            self.deleteButtonView = view;
+        }
         
     } else {
         [self.layer removeAnimationForKey:animationKey];
+        [UIView animateWithDuration:0.1 animations:^{
+            self.deleteButtonView.alpha = 0.0;
+        } completion:^(BOOL complete){
+            if (complete) {
+                [self.deleteButtonView removeFromSuperview];
+            }
+        }];
     }
     
+}
+
+@end
+
+@implementation DeleteButtonView
+
+- (id) initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.opaque = NO;
+        self.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    }
+    return self;
+}
+
+- (void) drawRect:(CGRect)rect {
+    
+    CGContextRef contex = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(contex, [UIColor colorWithWhite:0 alpha:0.5].CGColor);
+    CGContextFillRect(contex, rect);
+    
+    // draw X mark
+    CGContextSetStrokeColorWithColor(contex, [UIColor whiteColor].CGColor);
+    CGContextSetLineWidth(contex, 10.0f);
+    
+    CGFloat w  = MIN(self.bounds.size.width, self.bounds.size.height) / 6;
+    CGFloat x0 = self.bounds.size.width / 2  - w;
+    CGFloat y0 = self.bounds.size.height / 2 - w;
+    CGFloat x1 = self.bounds.size.width / 2  + w;
+    CGFloat y1 = self.bounds.size.height / 2 + w;
+    CGContextMoveToPoint(contex, x0, y0);
+    CGContextAddLineToPoint(contex, x1, y1);
+    CGContextMoveToPoint(contex, x0, y1);
+    CGContextAddLineToPoint(contex, x1, y0);
+    CGContextDrawPath(contex, kCGPathStroke);
 }
 
 @end
